a1778843b8f652b81688e406c4392b3b944da802
matMMI <mathis.togni@sfr.fr>
2023-04-10 17:33:58 +0200

    ajout page 404 error

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+// pages/404.js
+const NotFoundPage = () => {
+  return (
+    <>
+      <div class="error">
+        <div class="conte">
+          <h1 class="text" data-text="404 ERROR">
+            404 ERROR
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NotFoundPage;
