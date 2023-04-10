3a1c157f5f764a7373e7bd6711dae3d4cfce6598
matMMI <mathis.togni@sfr.fr>
2023-04-10 17:05:24 +0200

    ajout du title

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,9 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="fr">
         <Head>
+          <title>→ MATHIS TOGNI – WORDPRESS DEVELOPER & UX/UI DESIGNER</title>
           <link
             rel="shortcut icon"
             href="https://apiportfolio.mathistogni.fr/favicon.ico"
