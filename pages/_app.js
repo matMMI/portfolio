f7a0bf41940de2be5bddd5d446467e458a212c87
matMMI <mathis.togni@sfr.fr>
2023-04-21 09:22:37 +0200

    mise à jour header _app.js et _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import "../app/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>→ MATHIS TOGNI – WEB DEVELOPER & UX/UI DESIGNER</title>
+      </Head>
       <Component {...pageProps} />
     </>
   );
