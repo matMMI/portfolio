1c4367ab9568631e68a2ca7f43078c0ab415f7be
matMMI <mathis.togni@sfr.fr>
2023-04-21 14:36:27 +0200

    correction call scripts

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
+
 class MyDocument extends Document {
   render() {
     return (
@@ -16,31 +17,28 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-        var _paq = window._paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-          var u="//matomo.mathistogni.fr/";
-          _paq.push(['setTrackerUrl', u+'matomo.php']);
-          _paq.push(['setSiteId', '1']);
-          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();
-      `,
+                var _paq = window._paq = window._paq || [];
+                /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                _paq.push(['trackPageView']);
+                _paq.push(['enableLinkTracking']);
+                (function() {
+                  var u="//matomo.mathistogni.fr/";
+                  _paq.push(['setTrackerUrl', u+'matomo.php']);
+                  _paq.push(['setSiteId', '1']);
+                  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+                })();
+              `,
             }}
           />
         </Head>
         <body>
           <Main />
           <NextScript />
-          <Script src="/js/jqueryLatest.js" strategy="beforeInteractive" />
-          <Script src="/js/isotope.js" strategy="beforeInteractive" />
-          <Script src="/js/imageLoaded.js" strategy="beforeInteractive" />
-          <Script src="/js/main.js" strategy="beforeInteractive" />
         </body>
       </Html>
     );
   }
 }
+
 export default MyDocument;
