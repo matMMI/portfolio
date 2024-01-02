dec2527f7096072217859c968fb5bf326c21f0e4
matMMI <mathis.togni@sfr.fr>
2024-01-02 17:55:32 +0100

    maj title

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,13 @@
 "use client";
 import Footer from "../components/footer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import he from "he";
 import "../app/globals.scss";
 import Lenis from "@studio-freight/lenis";
 import Navigation from "../components/navigation";
 function Layout(props) {
+  const [pageTitle, setPageTitle] = useState(""); //
   useEffect(() => {
     const lenis = new Lenis();
     lenis.on("scroll", (e) => {});
@@ -14,19 +17,22 @@ function Layout(props) {
     }
     requestAnimationFrame(raf);
     const menuTrigger = document.querySelector(".menu-trigger");
+    axios
+      .get("https://apiportfolio.mathistogni.fr/wp-json/wp/v2/pages")
+      .then((response) => {
+        const title = response.data[0].title.rendered;
+        setPageTitle(title);
+      })
+      .catch((error) => {
+        console.error("Error fetching page title:", error);
+      });
   }, []);
   return (
     <>
       <header>
         <h1>
-          <span className="txt t1">
-            – MATHIS TOGNI – MASTER’S STUDENT AT INGEMEDIA – WEB DEVELOPER &
-            UX/UI DESIGNER&nbsp;
-          </span>
-          <span className="txt t2">
-            – MATHIS TOGNI – MASTER’S STUDENT AT INGEMEDIA – WEB DEVELOPER &
-            UX/UI DESIGNER&nbsp;
-          </span>
+          <span className="txt t1">{he.decode(pageTitle)}</span>
+          <span className="txt t2">{he.decode(pageTitle)}</span>
         </h1>
       </header>
       <main>
